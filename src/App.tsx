4de0320954d693b27633f191147f21ca872808d0
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-05-20 16:22:19 +0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import LoginPage from './components/auth/login'
+import Home from './components/home'
+import { Route, Routes } from 'react-router-dom'
+import PrivateRouter from './utils/router/privateRouter'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Routes>
+                <Route element={<PrivateRouter />}>
+                    <Route path="/" element={<Home />} />
+                </Route>
+                <Route path="login" element={<LoginPage />} />
+            </Routes>
+        </div>
+    )
 }
 
-export default App;
+export default App
