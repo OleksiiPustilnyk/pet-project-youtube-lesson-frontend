9d7879903f7e3baad904a2f2e3e53a4065d01dae
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-05-21 16:38:34 +0300

    Auth comonent - Create auth component logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import './App.css'
-import LoginPage from './components/auth/login'
 import Home from './components/home'
 import { Route, Routes } from 'react-router-dom'
 import PrivateRouter from './utils/router/privateRouter'
+import AuthRootComponent from './components/auth'
 
 function App() {
     return (
@@ -11,7 +10,8 @@ function App() {
                 <Route element={<PrivateRouter />}>
                     <Route path="/" element={<Home />} />
                 </Route>
-                <Route path="login" element={<LoginPage />} />
+                <Route path="login" element={<AuthRootComponent />} />
+                <Route path="register" element={<AuthRootComponent />} />
             </Routes>
         </div>
     )
---
+++ src/components/auth/register/index.tsx
@@ -0,0 +1,94 @@
+import { Button, TextField, Typography } from '@mui/material'
+import { IPropsRegister } from '../../../common/types/auth'
+
+const RegisterPage: React.FC<IPropsRegister> = (
+    props: IPropsRegister
+): JSX.Element => {
+    const {
+        setEmail,
+        setPassword,
+        setRepeatPassword,
+        setFirstName,
+        setUsername,
+        navigate,
+    } = props
+    return (
+        <>
+            <Typography variant="h2" fontFamily={'Poppins'} textAlign="center">
+                Регистрация
+            </Typography>
+            <Typography
+                variant="body1"
+                marginBottom={3}
+                fontFamily={'Poppins'}
+                textAlign="center"
+            >
+                Введите данные для регистрации
+            </Typography>
+            <TextField
+                fullWidth={true}
+                margin="normal"
+                label="Имя"
+                variant="outlined"
+                placeholder="Введите ваше имя"
+                onChange={(e) => setFirstName(e.target.value)}
+            />
+            <TextField
+                fullWidth={true}
+                margin="normal"
+                label="Username"
+                variant="outlined"
+                placeholder="Введите ваш username"
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <TextField
+                fullWidth={true}
+                margin="normal"
+                label="Email"
+                variant="outlined"
+                placeholder="Введите ваш email"
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+                fullWidth={true}
+                margin="normal"
+                label="Password"
+                variant="outlined"
+                type="password"
+                placeholder="Введите ваш пароль"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <TextField
+                fullWidth={true}
+                margin="normal"
+                label="Password"
+                variant="outlined"
+                type="password"
+                placeholder="Повторите ваш пароль"
+                onChange={(e) => setRepeatPassword(e.target.value)}
+            />
+            <Button
+                sx={{
+                    fontFamily: 'Poppins',
+                    marginTop: 2,
+                    marginBottom: 2,
+                    width: ' 60%',
+                }}
+                variant="contained"
+                type="submit"
+            >
+                Регистрация
+            </Button>
+            <Typography variant="body1" sx={{ fontFamily: 'Poppins' }}>
+                У вас есть аккаунта?
+                <span
+                    className="incidingText"
+                    onClick={() => navigate('/login')}
+                >
+                    Авторизация
+                </span>
+            </Typography>
+        </>
+    )
+}
+export default RegisterPage
