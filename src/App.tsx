b2c29c348e0e36293fdad50e437781b8b6c301f7
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-05-22 23:34:19 +0300

    Create sidebar - Settings sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,36 @@ import Home from './components/home'
 import { Route, Routes } from 'react-router-dom'
 import PrivateRouter from './utils/router/privateRouter'
 import AuthRootComponent from './components/auth'
+import { ColorModeContext, useMode } from './theme'
+import { ThemeProvider, CssBaseline } from '@mui/material'
+import LayoutComponent from './components/layout'
 
 function App() {
+    const [theme, colorMode] = useMode()
+
     return (
-        <div className="app">
-            <Routes>
-                <Route element={<PrivateRouter />}>
-                    <Route path="/" element={<Home />} />
-                </Route>
-                <Route path="login" element={<AuthRootComponent />} />
-                <Route path="register" element={<AuthRootComponent />} />
-            </Routes>
-        </div>
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <LayoutComponent>
+                    <div className="app">
+                        <Routes>
+                            <Route element={<PrivateRouter />}>
+                                <Route path="/" element={<Home />} />
+                            </Route>
+                            <Route
+                                path="login"
+                                element={<AuthRootComponent />}
+                            />
+                            <Route
+                                path="register"
+                                element={<AuthRootComponent />}
+                            />
+                        </Routes>
+                    </div>
+                </LayoutComponent>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
     )
 }
 
