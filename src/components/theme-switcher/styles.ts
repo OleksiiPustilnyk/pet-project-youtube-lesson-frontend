bcdeb67581e4ce638119678e8a422af5af8a2b43
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-05-30 17:07:21 +0300

    minor changes

--- src/components/theme-switcher/styles.ts
+++ src/components/theme-switcher/styles.ts
@@ -1,10 +1,7 @@
 import { makeStyles } from '@mui/styles'
 import { Theme } from '@mui/material'
-import { tokens } from '../../theme'
 
 export const useStyles = makeStyles((theme: Theme) => {
-    const colors = tokens(theme.palette.mode)
-
     return {
         iconBlock: {
             paddingRight: '35px',
