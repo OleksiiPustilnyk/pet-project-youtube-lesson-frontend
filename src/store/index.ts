e490d71a543cc07b969b1192844bcf1a5f059264
Oleksii Pustilnyk <pustilnyk.oleksii@gmail.com>
2024-06-04 10:42:00 +0300

    Implement News Logic - Create new component logic - Get news data from cryptocompare api - Render News record
    Implement alert logic in single asset page

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import authReducer from './slice/auth'
 import assetSlice from './slice/assets'
 import watchlistSlice from './slice/watchlist'
+import newsSlice from './slice/news'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
         assets: assetSlice,
         watchlist: watchlistSlice,
+        news: newsSlice,
     },
 })
 
